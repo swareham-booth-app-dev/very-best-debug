1290a01d987eaf7619479013a6e825d127d57ffd
swareham-booth <swareham@chicagobooth.edu>
2021-05-16 23:25:21 +0000

    Users working

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   get("/users", { :controller => "users", :action => "index" })
   get("/users/:username", { :controller => "users", :action => "show" })
   get("/insert_user_record", { :controller => "users", :action => "create" })
-  get("/update_users/:user_id", { :controller => "users", :action => "update" })
+  get("/update_user/:user_id", { :controller => "users", :action => "update" })
 
   get("/venues", { :controller => "venues", :action => "index" })
   get("/insert_venue_record", { :controller => "venues", :action => "create" })
