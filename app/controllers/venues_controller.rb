1d0303dba9624a53682ec748f8dd91f1829dfe9e
swareham-booth <swareham@chicagobooth.edu>
2021-05-16 23:52:28 +0000

    Venues view working

--- app/controllers/venues_controller.rb
+++ app/controllers/venues_controller.rb
@@ -1,8 +1,8 @@
 class VenuesController < ApplicationController
 
   def index
-    matching_venues = venue.all
-    venues = matching_venues.order(:created_at)
+    matching_venues = Venue.all
+    @venues = matching_venues.order(:created_at)
 
     render({ :template => "venue_templates/venue_list.html.erb" })
   end
@@ -10,7 +10,7 @@ class VenuesController < ApplicationController
   def show
     venue_id = params.fetch("venue_id")
     matching_venues = Venue.where({ :id => venue_id })
-    the_venue = matching_venues
+    @the_venue = matching_venues[0]
 
     render({ :template => "venue_templates/details.html.erb" })
   end
@@ -18,8 +18,8 @@ class VenuesController < ApplicationController
   def create
     venue = Venue.new
     venue.address = params.fetch("query_address")
-    venue.name = params.fetch("name")
-    venue.neighborhood = params.fetch("neighborhood")
+    venue.name = params.fetch("query_name")
+    venue.neighborhood = params.fetch("query_neighborhood")
     venue.save
 
     redirect_to("/venues/#{venue.name}")
